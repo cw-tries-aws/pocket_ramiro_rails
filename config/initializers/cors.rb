c3e4617645d6003449c7df31302021bd9f3f83e3
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-25 08:16:37 -0600

    Removes / at end of GCP origin in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -28,7 +28,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://pocket-ramiro-react.appspot.com/'
+    origins 'https://pocket-ramiro-react.appspot.com'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
